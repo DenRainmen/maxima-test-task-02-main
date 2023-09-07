95ffd1a8c1751f2361eb4ad0a16b54eaa01e4546
Den <potarykindenis@gmail.com>
2023-09-07 20:21:04 +0300

    Init commit

---
+++ maxima-test-task-02-main/script.js
@@ -0,0 +1,15 @@
+$('#slider').slick({
+	infinite: true,
+	prevArrow: '<div class="slick-btn-own slick-prev">&#8592;</div>',
+	nextArrow: '<div class="slick-btn-own slick-next">&#8594;</div>'
+});
+
+
+$(document).ready(function () {
+	
+	$("#open-create-user").click(function(){
+					$("#user-modal").fadeIn(200)
+		
+	})
+
+});
